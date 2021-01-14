ced40d046c7d9e927e4ee82d0da20bf9b33e259b
tSupan <t408805757@gmail.com>
2021-01-14 18:22:08 +0800

    学习中

--- src/component/topbar/topbar.js
+++
@@ -1,3 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/css/bootstrap-reboot.css'
-import 'bootstrap/dist/css/bootstrap-grid.css'
--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-<div id="topbar"></div>
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactBootstrap from 'react-bootstrap'
+
+
+class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {date:new Date()};
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(()=>this.tick(), 1000);
+    }
+    
+    componentDidUnMount(){
+        clearInterval(this.timerID);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>这是一个主页。</h1>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+}
+
+class Index extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {date:new Date()};
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(()=>this.tick(), 1000);
+    }
+    
+    componentDidUnMount(){
+        clearInterval(this.timerID);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>这是一个主页222。</h1>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('root'));
+
+ReactDOM.render(<Index />, document.getElementById('root'));
