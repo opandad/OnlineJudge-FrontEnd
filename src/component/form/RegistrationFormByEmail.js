c0cb306c2a794e96ed8e82896f7b0378ffd2d449
tSupan <t408805757@gmail.com>
2021-02-07 18:23:58 +0800

    注册登录界面和后端对接

--- src/component/form/RegistrationFormByEmail.js
+++ src/component/form/RegistrationFormByEmail.js
@@ -1,22 +1,84 @@
 import React, { Component } from 'react'
 import { SHA1 } from 'crypto-js'
 import { ws } from '../../utils/Websocket'
-import { Form, Input, Button, Checkbox, Row } from 'antd';
+import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
 import { MailOutlined, LoadingOutlined, CheckOutlined } from '@ant-design/icons'
 
-
 export class RegistrationFormByEmail extends Component {
-    
+    //样式
+    formItemLayout = {
+        labelCol: {
+            xs: {
+                span: 24,
+            },
+            sm: {
+                span: 8,
+            },
+        },
+        wrapperCol: {
+            xs: {
+                span: 24,
+            },
+            sm: {
+                span: 16,
+            },
+        },
+    };
 
-    sendVerifyCode(props){
+    tailFormItemLayout = {
+        wrapperCol: {
+            xs: {
+                span: 24,
+                offset: 0,
+            },
+            sm: {
+                span: 16,
+                offset: 8,
+            },
+        },
+    };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: ""
+        };
+        this.handleAccount = this.handleAccount.bind(this);
+        this.sendVerifyCode = this.sendVerifyCode.bind(this);
     }
-    
+
+    handleAccount(event){
+        this.setState({account: event.target.value});
+        console.log(this.state.account);
+    }
+
+    sendVerifyCode(event) {
+        var sendInfo={
+            requestPath:"user/regist/verifyCode/email",
+            user:{
+                account:this.state.account
+            }
+        };
+
+        console.log(sendInfo);
+
+        ws.send(JSON.stringify(sendInfo));
+    }
+
+    //没完成
+    onFinish = (values) => {
+
+    };
+
+    onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
     render() {
         return (
             <Row justify="center">
                 <Form
-                    {...this.layout}
+                    {...this.formItemLayout}
                     name="loginByEmail"
                     initialValues={{
                         remember: true,
@@ -32,7 +94,7 @@ export class RegistrationFormByEmail extends Component {
                             },
                         ]}
                     >
-                        <Input />
+                        <Input onChange={this.handleAccount} />
                     </Form.Item>
                     <Form.Item
                         label="密码"
@@ -49,11 +111,20 @@ export class RegistrationFormByEmail extends Component {
                     <Form.Item
                         label="确认密码"
                         name="repeatPassword"
+                        dependencies={['password']}
                         rules={[
                             {
                                 required: true,
-                                message: '请再一次输入密码！',
+                                message: '请再一次输入相同密码！',
                             },
+                            ({ getFieldValue }) => ({
+                                validator(_, value) {
+                                    if (!value || getFieldValue('password') === value) {
+                                        return Promise.resolve();
+                                    }
+                                    return Promise.reject('两次密码不相同！');
+                                },
+                            }),
                         ]}
                     >
                         <Input.Password />
@@ -68,18 +139,23 @@ export class RegistrationFormByEmail extends Component {
                             },
                         ]}
                     >
-                        <Input />
-                        <Button onClick={this.sendVerifyCode} icon={<MailOutlined />}>
-                            发送验证码
-                        </Button>
+                        <Row gutter={8}>
+                            <Col span={14}>
+                                <Input />
+                            </Col>
+                            <Col span={6}>
+                                <Button onClick={this.sendVerifyCode} icon={<MailOutlined />}>
+                                    发送验证码
+                                </Button>
+                            </Col>
+                        </Row>
                     </Form.Item>
-                    <Form.Item {...this.tailLayout} name="remember" valuePropName="checked">
-                        <Checkbox>自动登录</Checkbox>
-                    </Form.Item>
-                    <Form.Item {...this.tailLayout}>
-                        <Button type="primary" htmlType="submit">
-                            注册
-                        </Button>
+                    <Form.Item>
+                        <Row span={10}>
+                            <Button type="primary" htmlType="submit">
+                                注册
+                            </Button>
+                        </Row>
                     </Form.Item>
                 </Form>
             </Row>
