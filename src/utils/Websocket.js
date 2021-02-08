0a54c95d642aa2b2aa5c75a639ddf2c9802c60bb
tSupan <t408805757@gmail.com>
2021-02-08 18:30:54 +0800

    websocket改进

--- src/utils/Websocket.js
+++ src/utils/Websocket.js
@@ -52,3 +52,59 @@ ws.onclose = (e) => {
     // document.cookie = "userID="+stringify(websocketData["userID"]);
     // Cookies.set('userInfo', websocketData["userID"], {expires: expires});
 };
+
+
+/*
+<===========================下面是改良版，正在写代码中=====================================>
+*/
+
+class OJWebSocket{
+    constructor(){
+        this.websocket = null;
+        
+        this.handlers = {
+            
+        };
+    }
+
+    initialize = () =>{
+        try{
+            this.websocket = new WebSocket("wss://" + REAREND_HOSTNAME);
+        }catch(err){
+            console.log("start websocket error: ", err);
+            setTimeout(()=>{
+                this.initialize();
+            }, 10000);
+        }
+ 
+        this.websocket.onopen = (e)=>{
+            //打开连接后自动登录
+            var loginInfo = {
+                websocketID: "",
+                RequestPath: "user/login/auto",
+                user:{
+                    userID:0,
+                    password:""
+                }
+            };
+            this.sendData(loginInfo);
+        };
+
+        this.websocket.onmessage=(e)=>{
+            var receive = JSON.parse(e.data);
+            
+        }
+
+        this.websocket.onerror = (e) =>{
+
+        };
+
+        this.websocket.onclose = (e) =>{
+
+        };
+
+        sendData = (message) =>{
+            this.websocket.send(JSON.stringify(message));
+        };
+    }
+}
